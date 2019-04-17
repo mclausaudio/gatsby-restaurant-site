dc0bf48dbd9f8e15e482cea14d45e63954f5b62a
Michael_Claus <mclausaudio@gmail.com>
2019-04-17 11:27:38 -0700

    first commit.  gatsby new project init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Eatery`,
+    description: `Restaurant Website built with GatsbyJS`,
+    author: `Michael Claus`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
