751830913685d6cc5f03541e076dddfb2682d028
Michael_Claus <mclausaudio@gmail.com>
2019-04-18 11:11:58 -0700

    cleaned up gatsby init, installed styled components, react icons, added bootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
